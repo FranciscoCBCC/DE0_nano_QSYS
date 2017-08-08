3f547c1e7b222e8aef60607fb1d970c1f8a98155
Francisco <francisco.nascimento@icen.ufpa.br>
2017-08-08 11:55:04 -0300

    Algoritmo genético funcionando, porém não extá exibindo os dados.

--- db/ip/sldb57f5f72/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sldb57f5f72/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 49312ec45466cb680244
+//   DESIGN_HASH   : 0d82cfcc4e9312da9c70
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h49312ec45466cb680244 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h0d82cfcc4e9312da9c70 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
--- output_files/DE0_NANO_QSYS.sld
+++ output_files/DE0_NANO_QSYS.sld
@@ -2,7 +2,7 @@
   <sld_infos>
     <sld_info hpath="" name="">
       <assignment_values>
-        <assignment_value text="QSYS_NAME de0_nano HAS_SOPCINFO 1 GENERATION_ID 1499088849"/>
+        <assignment_value text="QSYS_NAME de0_nano HAS_SOPCINFO 1 GENERATION_ID 1502201900"/>
       </assignment_values>
     </sld_info>
     <sld_info hpath="sld_hub:auto_hub|alt_sld_fab:\instrumentation_fabric_with_node_gen:instrumentation_fabric" library="alt_sld_fab" name="instrumentation_fabric">
