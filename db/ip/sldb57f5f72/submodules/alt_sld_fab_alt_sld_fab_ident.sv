785cca88cff5e13cea40d9a43491ed1864c7ced3
Francisco <francisco.nascimento@icen.ufpa.br>
2017-08-10 12:35:14 -0300

    Adição do SDRAM

--- db/ip/sldb57f5f72/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sldb57f5f72/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 0d82cfcc4e9312da9c70
+//   DESIGN_HASH   : 298f4f0e5b3e6934261f
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h0d82cfcc4e9312da9c70 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h298f4f0e5b3e6934261f };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
